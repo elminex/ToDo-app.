dee610c337e3b0fc3c3a3ea2911bc48b9041e98c
Paweł Tuliński <pawel.tulinski@gmail.com>
2019-05-16 21:16:16 +0200

    Add files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/app.js',
+module.exports = (env) => {
+  const enviroment = env || 'production';
+  return {
+    mode: enviroment,
+    entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
-    }
+      path: path.resolve(__dirname, 'build'),
+      filename: `app.${enviroment}.bundle.js`,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.css$/,
+          use: [
+            { loader: 'style-loader' },
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    optimization: {
+      minimize: false,
+    },
+  };
 };
